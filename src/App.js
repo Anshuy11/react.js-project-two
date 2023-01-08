55c0d127bf5ddc8bde69b83e930929110322b5f3
Anshu <anshuy115@gmail.com>
2023-01-08 17:18:23 +0530

    "react.js, chakra-ui dual mode website"

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Videos from "./components/Videos";
+import Upload from "./components/Upload";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+
+function App() {
+ return(
+
+<Router>
+
+<Header />
+
+<Routes>
+
+<Route path="/" element={<Home/>}   />
+<Route path="/videos" element={<Videos/>}   />
+<Route path="/upload" element={<Upload/>}   />
+<Route path="/signup" element={<Signup/>}   />
+<Route path="/login" element={<Login/>}   />
+
+
+</Routes>
+
+<Footer />
+
+</Router>
+
+ )
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Box,Stack,Heading,VStack, HStack, Button,Input,Text} from '@chakra-ui/react';
+import {AiOutlineSend} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+   <Box bgColor={'blackAlpha.900'} minH={'30'}
+   p="4"
+   color={'white'}
+   >
+   <Stack direction={['column','row']}>
+<VStack alignItems={'stretch'} width={'full'} py={'4'} px={"2"}>
+   <Heading size={'md'} textTransform={'capitalize'} textAlign={'center'} >
+    Subscribe for getting updates regularly.
+   </Heading>
+
+   <HStack 
+   borderBottom={'none'}
+   >
+
+   <Input  placeholder='Enter your Email...' border={'none'} outline={'solid'} color='purple'/>
+   <Button 
+   p={'0'}
+   colorScheme={'purple'}
+   variant={'ghost'}
+   borderRadius={'0 20px 20px 0'}
+   >
+
+   <AiOutlineSend />
+
+   </Button>
+   </HStack>
+   </VStack>
+
+   <VStack w={'full'}
+   borderLeft={['none','1px solid white']}
+   borderRight={['none','1px solid white']}
+   >
+
+   <Heading textTransform={'capitalize'}
+     textAlign={'center'} size={'md'}>
+     Video Hub
+     </Heading>
+
+     <Text> &#169;All rights resevered</Text>
+   </VStack>
+   
+   <VStack w={'full'} >
+   
+     <Heading size={'md'} textTransform={'capitalize'}>Social Media</Heading>
+
+    <Button variant={'link'} colorScheme={'purple'}> <a target={'blank'} href ='https://youtube.com'>Youtube</a></Button>
+   
+    <Button variant={'link'} colorScheme={'purple'}> <a target={'blank'} href ='https://instagram.com'>Instagram</a></Button>
+  
+    <Button variant={'link'} colorScheme={'purple'}> <a target={'blank'} href ='https://github.com'>GitHub</a></Button>
+   </VStack>
+   
+   </Stack>
+   
+   
+   </Box>
+  )
+}
+
+export default Footer
